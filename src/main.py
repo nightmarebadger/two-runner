21fe1c3464392934a4741771ed49b3ecedadf5b8
Matej <matej@matej-Compaq-610.(none)>
2013-11-28 15:34:17 +0100

    Fixing some problems and adding new images

--- src/main.py
+++ src/main.py
@@ -13,12 +13,19 @@ class Game(cocos.layer.ColorLayer):
         self.player.position = 25, 25
         self.player.velocity = 0, 0
         self.player.speed = 500
-        self.player.gravity = -1500
-        self.add(self.player, z=0)
+        self.player.gravity = -1300
+        self.add(self.player, z = 0)
         self.player.do(Move())
         self.player.jumping = False
+        self.obstacle = cocos.sprite.Sprite(resources.obstacle)
+        self.obstacle.position = 770,30
+        self.obstacle.velocity = -100, 0
+        self.obstacle.speed = 50
+        self.add(self.obstacle, z = 0)
+        self.obstacle.do(Move())
 
-        self.schedule(self.update)
+
+        self.player.schedule(self.update)
 
     def on_key_press(self, symbol, modifiers):
         if symbol == key.LEFT:
@@ -32,7 +39,7 @@ class Game(cocos.layer.ColorLayer):
         elif symbol == key.DOWN:
             self.player.velocity = self.player.velocity[0], -self.player.speed
         elif symbol == key.SPACE:
-            self.player.velocity = 0, 0
+           self.player.velocity = 0, 0
 
     def on_key_release(self, symbol, modifier):
         if symbol == key.LEFT:
@@ -41,25 +48,87 @@ class Game(cocos.layer.ColorLayer):
             self.player.velocity = 0, self.player.velocity[1]
 
     def update(self, dt):
-        if self.player.position[1] < 25:
+        if self.player.position[1] <= 25:
             self.player.position = (self.player.position[0], 26)
             self.player.velocity = (self.player.velocity[0], 0)
             self.player.jumping = False
         elif self.player.position[0] < 25:
-            self.player.position = (26, self.player.position[1])
+           self.player.position = (26, self.player.position[1])
         elif self.player.position[0] > 775:
             self.player.position = (775, self.player.position[1])
-        
         else:
             pass
 
-        
+class MainMenu(cocos.menu.Menu):
+    def __init__(self):
+        super(MainMenu, self).__init__()
+
+        self.font_title['font_name'] = 'Edit Undo Line BRK'
+        self.font_title['font_size'] = 52
+        self.font_title['color'] = (240, 0, 220, 255)
+
+        self.font_item['color'] = (255, 255, 255, 255)
+        self.font_item_selected['color'] = (240, 0, 220, 255)
+
+        items = []
+
+        items.append(cocos.menu.MenuItem('New game', self.on_new_game))
+        items.append(cocos.menu.MenuItem('Options', self.on_options))
+        items.append(cocos.menu.MenuItem('Quit', self.on_quit))
+
+        self.create_menu(items, cocos.menu.shake(), cocos.menu.shake_back())
+
+    def on_new_game(self):
+        game_layer = Game()
+        game_scene = cocos.scene.Scene(game_layer)
+
+        cocos.director.director.push(game_scene)
+
+    def on_options(self):
+        self.parent.switch_to(1)
+
+    def on_quit(self):
+        pyglet.app.exit()
+
+class OptionsMenu(cocos.menu.Menu):
+    def __init__(self):
+        super(OptionsMenu, self).__init__("two_runner")
+
+        self.font_title["font_name"] = "Edit Undo Line BRK"
+        self.font_title["font_size"] = 52
+        self.font_title["color"] = (240, 0, 220, 255)
+
+        self.font_item["color"] = (255, 255, 255, 255)
+        self.font_item_selected["color"] = (240, 0, 220, 255)
+
+        items = []
+        items.append(cocos.menu.MenuItem("Fullscreen", self.on_fullscreen))
+        items.append(cocos.menu.MenuItem("Back", self.on_quit))
+        self.create_menu(items, cocos.menu.shake(), cocos.menu.shake_back())
+
+    def on_fullscreen(self):
+        cocos.director.director.window.set_fullscreen(not cocos.director.director.window.fullscreen)
+
+    def on_quit(self):
+        self.parent.switch_to(0)
+
+class BackgroundLayer(cocos.layer.Layer):
+    def __init__(self):
+        super(BackgroundLayer, self).__init__()
+
+        self.image = cocos.sprite.Sprite(resources.background)
+        self.image.position = 400, 250
+        self.add(self.image, z = 0)
+
+        self.player = cocos.sprite.Sprite(resources.mario)
+        self.player.position = 25, 25
+        self.add(self.player, z = 1)
+
 if __name__ == '__main__':
-    cocos.director.director.init(
-        width=800,
-        height=500)
-    
+
+    cocos.director.director.init(width = 800, height = 500)
     scene = cocos.scene.Scene()
-    scene.add(Game(), z = 0)
+    scene.add(cocos.layer.MultiplexLayer(MainMenu(), OptionsMenu()), z = 1)
+    scene.add(BackgroundLayer(), z = 0)
     cocos.director.director.run(scene)
     
--- src/resources.py
+++ src/resources.py
@@ -4,3 +4,5 @@ pyglet.resource.path = ["resources"]
 pyglet.resource.reindex()
 
 mario = pyglet.resource.image("mario.jpg")
+obstacle = pyglet.resource.image("obstacle.png")
+background = pyglet.resource.image("background.jpg")
